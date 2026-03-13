343fd203eb4ef0985519182f5babc08a687e8ff2
Joaquin <joaquin_eleuce@hotmail.com>
2026-03-12 23:05:49 -0500

    patch

--- src/app/features/auth/pages/auth-wrapper/auth-wrapper.component.ts
+++ src/app/features/auth/pages/auth-wrapper/auth-wrapper.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-auth-wrapper',
   templateUrl: './auth-wrapper.component.html',
   styleUrls: ['./auth-wrapper.component.scss'],
 })
-export class AuthWrapperComponent {}
+export class AuthWrapperComponent implements OnInit, OnDestroy {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
+  ) {}
+
+  ngOnInit(): void {
+    this.renderer.addClass(this.document.body, 'auth-page');
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(this.document.body, 'auth-page');
+  }
+}
